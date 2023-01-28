2c692b86efda81b59ab17b8212a4d4e8ec811a7f
AZYsGithub <114379782+AZYsGithub@users.noreply.github.com>
2023-01-29 04:11:10 +0800

    Update Chat Bypass - ChatSplitter(Source).lua

--- Chat Bypass - ChatSplitter(Source).lua
+++ Chat Bypass - ChatSplitter(Source).lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/chillz-basement/main/Chat%20Bypasser?token=GHSAT0AAAAAAB5XXBHFH4T7ORSLGHK7RSLEY6VQCWQ")()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/chillz-basement/main/Chat%20Bypasser?token=GHSAT0AAAAAAB5XXBHFCLETI2FMWP45BTRMY6VQFTA")()
